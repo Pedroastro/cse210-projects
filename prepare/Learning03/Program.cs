1e1974892ec0982c5d2305bb8cd269e197f0e5d8
pedroastro <pedromscastro9095@gmail.com>
2024-10-02 20:35:50 -0300

    add week 3 starting assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // 1
+        Fraction one = new Fraction();
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+
+        // 5
+        Fraction five = new Fraction(5);
+        Console.WriteLine(five.GetFractionString());
+        Console.WriteLine(five.GetDecimalValue());
+
+        // 3/4
+        Fraction threeOverFour = new Fraction(3,4);
+        Console.WriteLine(threeOverFour.GetFractionString());
+        Console.WriteLine(threeOverFour.GetDecimalValue());
+
+        // 1/3
+        Fraction oneThird = new Fraction(1,3);
+        Console.WriteLine(oneThird.GetFractionString());
+        Console.WriteLine(oneThird.GetDecimalValue());
     }
 }
