593e79c06ef3720a5ee4fb88a3c8d24a6dab94c0
pedroastro <pedromscastro9095@gmail.com>
2024-10-06 19:27:55 -0300

    remove test file

--- prove/Develop03/test.py
+++
@@ -1,5 +0,0 @@
-import json
-
-with open("C:/Users/Pedro Castro/Desktop/BYU/CSE 210/cse210-projects/prove/Develop03/t_kjv.json") as file:
-    data = json.load(file)
-    print(data['row'][0]['field'][-1])
