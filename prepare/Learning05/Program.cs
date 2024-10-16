7b3fb535e88d7aeb16317b6b2fbb83ae12e6e809
pedroastro <pedromscastro9095@gmail.com>
2024-10-16 00:26:44 -0300

    add week 5 learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment testAssignment = new Assignment("Pedro", "Math");
+        Console.WriteLine(testAssignment.GetSummary());
+
+        MathAssignment testMathAssignment = new MathAssignment("Pedro", "Math", "Section 1", "Problem 2");
+        Console.WriteLine(testMathAssignment.GetSummary());
+        Console.WriteLine(testMathAssignment.GetHomeworkList());
+
+        WritingAssignment testWritingAssignment = new WritingAssignment("Pedro", "Writing", "My Novel");
+        Console.WriteLine(testWritingAssignment.GetSummary());
+        Console.WriteLine(testWritingAssignment.GetWritingInformation());
     }
 }
